76e90da52c760392604787c3254f0d6e04bfbfb5
manu <manu.learnerdevops@gmail.com>
2022-11-02 00:19:11 +0530

    started

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="logo-size">
+        {" "}
+        <img src={logo} alt="LOGO" />
+      </div>
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/User.js
@@ -0,0 +1,9 @@
+function User() {
+    return (
+      <div className="App">
+       <h1>hello User</h1>
+      </div>
+    );
+  }
+  
+  export default User;
