a7a233b34842ccbdc4ac1ff360a64a18c75396e3
manu <manu.learnerdevops@gmail.com>
2022-11-02 01:09:21 +0530

    -

--- src/User.js
+++ src/User.js
@@ -1,9 +1,10 @@
 function User() {
-    return (
-      <div className="App">
-       <h1>hello User</h1>
-      </div>
-    );
-  }
-  
-  export default User;
+  return (
+    //*Start https://prod.liveshare.vsengsaas.visualstudio.com/join?39FE850FEE075A39FF8B34C9BAD4143EC650
+    <div className="App">
+      <h1>hello User</h1>
+    </div>
+  );
+}
+
+export default User;
